87c494d13a255a481a90b4fd5aab0bc2a5101346
Michael Schilling <mschilling@arkid.nl>
2018-07-27 08:47:57 +0200

    Re-format main entrypoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,53 +1,46 @@
 import * as functions from 'firebase-functions';
 
-import {
-    dialogflow,
-    SimpleResponse,
-} from 'actions-on-google';
+import { dialogflow, SimpleResponse } from 'actions-on-google';
 
 import { ssml as ssmlText } from './templates/default';
 
-const format = require("string-template");
+const format = require('string-template');
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-
-const app = dialogflow()
+const app = dialogflow();
 
 app.intent('N Times Tables', conv => {
+  console.log('parameters', conv.parameters);
 
-    console.log('parameters', conv.parameters);
+  const maxTimes = 10;
 
-    const maxTimes = 10;
+  let N = 7;
+  try {
+    N = parseInt(conv.parameters.n + '');
+  } catch (error) {}
 
-    let N = 7;
-    try {
-        N = parseInt(conv.parameters.n + '');
-    } catch(error){
+  let tableText = '';
+  let tableSpeech = `<speak>Hier komt de tafel van ${N}.</speak>`;
 
-    }
+  for (let i = 1; i <= maxTimes; i++) {
+    tableText += `${i} x ${N} = ${i * N}\n`;
+    tableSpeech += `<speak>${i} maal ${N} = ${i *
+      N}</speak><break time="0.3s" strength="strong"/>`;
+  }
 
-    let tableText = '';
-    let tableSpeech = `<speak>Hier komt de tafel van ${N}.</speak>`;
+  const values: number[] = [];
+  for (let i = 1; i <= maxTimes; i++) {
+    values.push(i * N);
+  }
 
-    for (let i = 1; i <= maxTimes; i++) {
-        tableText += `${i} x ${N} = ${(i * N)}\n`;
-        tableSpeech += `<speak>${i} maal ${N} = ${(i * N)}</speak><break time="0.3s" strength="strong"/>`;
-    }
+  const speechText = format(ssmlText, [N, ...values]);
 
-    const values: number[] = [];
-    for (let i = 1; i <= maxTimes; i++) {
-        values.push(i * N);
-    }
+  // let response =
+  conv.close(
+    new SimpleResponse({
+      speech: speechText,
+      text: tableText,
+    })
+  );
+});
 
-    const speechText = format(ssmlText, [ N, ...values]);
-
-
-    // let response = 
-    conv.close(new SimpleResponse({
-        speech: speechText,
-        text: tableText
-    }))
-
-})
-
-exports.assistant = functions.https.onRequest(app)
+exports.assistant = functions.https.onRequest(app);
